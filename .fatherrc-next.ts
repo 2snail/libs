4df01fb6aefc267a0899bd1355179a6b9015624d
zhangaz1 <zhangaz1@hotmail.com>
2020-09-19 17:00:23 +0800

    remove father-next use tsc

--- .fatherrc-next.ts
+++
@@ -1,5 +0,0 @@
-export default {
-	// platform: 'node',
-	// bundle: true,
-	formats: ['cjs', 'esm', ['umd', { globalName: 'libs' }]],
-};
---
+++ example/src/fp/index.ts
@@ -0,0 +1,5 @@
+import { fp } from '@2snail/libs';
+
+export const add = (a: number, b: number) => {
+	fp.option.some(a);
+};
