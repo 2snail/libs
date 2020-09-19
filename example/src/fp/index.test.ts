d34f301ddccf5332165a16a829a7baba971a2876
zhangaz1 <zhangaz1@hotmail.com>
2020-09-19 17:37:32 +0800

    update tsc to es5 from es6 fix node import error

--- example/src/fp/index.test.ts
+++ example/src/fp/index.test.ts
@@ -1,4 +1,4 @@
-import * as fp from './index';
+import { fp } from '@2snail/libs';
 
 describe('fp', () => {
 	test('fp', () => {
--- example/src/fp/index.ts
+++
@@ -1,5 +0,0 @@
-import { fp } from '@2snail/libs';
-
-export const add = (a: number, b: number) => {
-	fp.option.some(a);
-};
